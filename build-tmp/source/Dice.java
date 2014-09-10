7b7f3ac1aaf23cefb9d5e045dca706213bb2e4f8
shleewhite <lwhite@sportsbasement.com>
2014-09-10 10:45:27 -0700

    Made dice roll

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,13 +1,29 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 Die one;
 Die two;
-void setup()
+public void setup()
 {
 	noLoop();
 	size(400, 400);
 	one = new Die(100, 100);
 	two = new Die(300, 100);
 }
-void draw()
+public void draw()
 {
 	background(255, 255, 255);
 	one.show();
@@ -15,7 +31,7 @@ void draw()
 	two.show();
 	two.roll();
 }
-void mouseClicked()
+public void mouseClicked()
 {
 	redraw();
 }
@@ -28,11 +44,11 @@ class Die
 		myX = x;
 		myY = y;
 	}
-	void roll()
+	public void roll()
 	{
 		numberDots = (int)((Math.random()*6)+1);
 	}
-	void show()
+	public void show()
 	{
 		rect(myX, myY, 75, 75);
 		if (numberDots == 1)
@@ -76,3 +92,12 @@ class Die
 		}
 	}
 }      
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
